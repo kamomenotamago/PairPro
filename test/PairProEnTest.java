830e44f57ba775f5bf276bc51367d7bcd781addc
g13929mr <g13929mr@gm.tsuda.ac.jp>
2015-05-08 11:21:11 +0900

    じゅうからten（途中）

--- test/PairProEnTest.java
+++ test/PairProEnTest.java
@@ -12,5 +12,12 @@ public class PairProEnTest {
 		String actual = en.translateEng(1);
 		assertThat(actual,is(expected));
 	}
-
+	
+	@Test
+	public void ‚¶‚ã‚¤‚©‚çten() {
+		IntoToEng en = new IntoToEng();
+		String expected = "ten";
+		String actual = en.translateEng(10);
+		assertThat(actual,is(expected));
+	}
 }
